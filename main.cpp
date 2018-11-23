0a5eeb7d42085036c8be88f80fd97770add37337
Joni Toiviainen <j.toiviainen@hotmail.co.uk>
2018-11-23 18:11:07 +0200

    Added a support for creating the Direct3D device and device context.

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@
 
 #include <d2d1.h>
 #include <d2d1_1.h>
+#include <d3d11.h>
 
 #include <cassert>
 #include <string>
@@ -15,13 +16,22 @@
 using namespace Microsoft::WRL;
 
 #pragma comment(lib, "d2d1.lib")
+#pragma comment(lib, "d3d11.lib")
 
 // ============================================================================
 
 constexpr auto WINDOW_CLASS_NAME = "D2D-SANDBOX";
 constexpr auto WINDOW_WIDTH = 800;
 constexpr auto WINDOW_HEIGHT = 600;
- 
+
+// ============================================================================
+
+struct D3DContext
+{
+  ComPtr<ID3D11Device> device;
+  ComPtr<ID3D11DeviceContext> deviceCtx;
+};
+
 // ============================================================================
 
 HWND gHwnd = nullptr;
@@ -181,6 +191,79 @@ ComPtr<ID2D1Factory> createD2DFactory()
 }
 
 // ============================================================================
+// Create a new Direct3D device and device context objects.
+//
+// Direct2D requires Direct3D device to construct a device context object. This
+// device is associated with the Direct2D device when creating the context. Note
+// that this construction is similar to typical clear Direct3D but with a twist.
+// 
+// Direct3D does allow applications to create devices which may emulate how the
+// real supported device would work. Here's the list of possible driver types.
+//   D3D_DRIVER_TYPE_NULL........A D3D driver without rendering support.
+//   D3D_DRIVER_TYPE_UNKNOWN.....The driver type is unknown.
+//   D3D_DRIVER_TYPE_SOFTWARE....Software emulation.
+//   D3D_DRIVER_TYPE_REFERENCE...Software emulation with full D3D support.
+//   D3D_DRIVER_TYPE_HARDWARE....A hardware driver which implements Direct3D.
+//   D3D_DRIVER_TYPE_WARP........High performance software rasterizer.
+//
+// Direct3D device must be initialised with an additional color channel support
+// so it can be correctly used with the Direct2D. This is because Direct3D uses
+// different channel ordering than Direct2D by default. Application should set
+// the D3D11_CREATE_DEVICE_BGRA_SUPPORT value as a creation flag. Here's the
+// full list of creation flags that can be used with Direct3D device creation.
+//   D3D11_CREATE_DEVICE_BGRA_SUPPORT..........
+//   D3D11_CREATE_DEVICE_DEBUG.................Enable the D3D debug layer.
+//   D3D11_CREATE_DEVICE_DEBUGGABLE............Enable shader debugger.
+//   D3D11_CREATE_DEVICE_DISABLE_GPU_TIMEOUT...Disable GPU timeout detection.
+//   D3D11_CREATE_DEVICE_PREVENT_ALTERING_*....Prevent the use of D3D debugger.
+//   D3D11_CREATE_DEVICE_PREVENT_INTERNAL_*....Disable WARP multithreading.
+//   D3D11_CREATE_DEVICE_SINGLETHREADED........Optimize for singlethread usage.
+//   D3D11_CREATE_DEVICE_SWITCH_TO_REF.........[Not supported]
+//   D3D11_CREATE_DEVICE_VIDEO_SUPPORT.........Fail if non-WDDM 1.2 compatible.
+// ============================================================================
+D3DContext createD3DContext()
+{
+  // specify the desired additional behavior how the device will be created.
+  UINT flags = 0;
+  flags |= D3D11_CREATE_DEVICE_BGRA_SUPPORT; // for Direct2D compatibility
+  flags |= D3D11_CREATE_DEVICE_SINGLETHREADED;
+  #ifdef _DEBUG
+  flags |= D3D11_CREATE_DEVICE_DEBUG;
+  #endif
+
+  // specify the feature levels we want to support (ordering matters!).
+  D3D_FEATURE_LEVEL featureLevels[] = 
+  {
+    D3D_FEATURE_LEVEL_11_1,
+    D3D_FEATURE_LEVEL_11_0,
+    D3D_FEATURE_LEVEL_10_1,
+    D3D_FEATURE_LEVEL_10_0,
+    D3D_FEATURE_LEVEL_9_3,
+    D3D_FEATURE_LEVEL_9_2,
+    D3D_FEATURE_LEVEL_9_1
+  };
+
+  // construct a new Direct3D device and a device context.
+  ComPtr<ID3D11Device> device;
+  ComPtr<ID3D11DeviceContext> deviceContext;
+  throwOnFail(D3D11CreateDevice(
+    nullptr,
+    D3D_DRIVER_TYPE_HARDWARE,
+    0,
+    flags,
+    featureLevels,
+    ARRAYSIZE(featureLevels),
+    D3D11_SDK_VERSION,
+    device.GetAddressOf(),
+    nullptr,
+    deviceContext.GetAddressOf()
+  ));
+
+  // return a Direct3D context containing the created objects.
+  return { device, deviceContext };
+}
+
+// ============================================================================
 
 int main()
 {
@@ -193,6 +276,7 @@ int main()
 
   // initialise Direct2D framework.
   auto factory = createD2DFactory();
+  auto d3dCtx = createD3DContext();
 
   // start the main loop of the application.
   MSG msg = {};
